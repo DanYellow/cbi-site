f9f18e63236619e398d8081c79bb43599b27c130
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-02-11 20:24:09 +0100

    Add template for email account activation

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -5,8 +5,12 @@ namespace App\EventSubscriber;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+use Symfony\Component\Mime\Address;
+
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
@@ -31,16 +35,23 @@ class EasyAdminSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $email = (new Email())
-            ->from('hello@example.com')
-            ->to($entity->getEmail())
-            ->subject('Votre compte a été activé')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+        if ($entity->isVerified()) {
+            try {
+                $email = (new TemplatedEmail())
+                    // ->from(new Address('hello@example.com', 'Club des Belles Images'))
+                    ->to($entity->getEmail())
+                    ->subject('Votre compte a été activé')
+                    ->htmlTemplate('emails/account_activation.html.twig')
+                    ->context([
+                        'user' => $entity,
+                    ])
+                ;
 
-        $this->mailer->send($email);
-        // dd($entity->isVerified());
-        // $slug = $this->slugger->slugify($entity->getTitle());
-        // $entity->setIsVerified(false);
+                $this->mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                // some error prevented the email sending; display an
+                // error message or try to resend the message
+            }
+        }
     }
 }
