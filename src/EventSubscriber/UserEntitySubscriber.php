85c0f72dfe6b4f9f3dd68a4a6b9c52aac9035d0b
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-02-12 15:51:47 +0100

    Send email when new member join the site

--- src/EventSubscriber/UserEntitySubscriber.php
+++ src/EventSubscriber/UserEntitySubscriber.php
@@ -9,17 +9,27 @@ use Doctrine\ORM\Events;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Mime\Address;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsEntityListener(event: Events::postUpdate, method: 'postUpdate', entity: User::class)]
+#[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: User::class)]
 class UserEntitySubscriber
 {
     protected MailerInterface $mailer;
+    protected ParameterBagInterface $params;
+    protected UserPasswordHasherInterface $userPasswordHasher;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(
+        MailerInterface $mailer,
+        ParameterBagInterface $params,
+        UserPasswordHasherInterface $userPasswordHasher
+    )
     {
         $this->mailer = $mailer;
+        $this->params = $params;
+        $this->userPasswordHasher = $userPasswordHasher;
     }
 
     public function postUpdate(User $user, PostUpdateEventArgs $args): void
@@ -31,7 +41,7 @@ class UserEntitySubscriber
             try {
                 $email = (new TemplatedEmail())
                     ->to(new Address($user->getEmail(), "{$user->getFirstname()} {$user->getLastname()}"))
-                    ->subject('Votre compte sur le site du Club des Belles Images a été activé')
+                    ->subject('CBI - Votre compte sur le site du Club des Belles Images a été activé')
                     ->htmlTemplate('emails/account_activation.html.twig')
                     ->context([
                         'user' => $user,
@@ -42,6 +52,34 @@ class UserEntitySubscriber
                 // some error prevented the email sending; display an
                 // error message or try to resend the message
             }
+        }
+    }
+
+    // public function preUpdate(User $user, PreUpdateEventArgs $args): void
+    // {
+    //     $entityManager = $args->getObjectManager();
+
+    //     $user->setPassword($this->userPasswordHasher->hashPassword($user, $user->getPassword()));
+    //     $entityManager->persist($user);
+    //     $entityManager->flush();
+    // }
+
+    public function postPersist(User $user): void
+    {
+        // container->getParameter("app.auto_email")
+        try {
+            $email = (new TemplatedEmail())
+                ->to(new Address($this->params->get("app.auto_email"), "Club des Belles Images"))
+                ->subject("CBI - {$user->getFullName()} vient de créer un compte")
+                ->htmlTemplate('emails/account_creation.html.twig')
+                ->context([
+                    'user' => $user,
+                ]);
+
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            // some error prevented the email sending; display an
+            // error message or try to resend the message
         }
     }
 }
