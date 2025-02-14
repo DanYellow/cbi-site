b065edcdb3ae9b8bab1087ff228d0e307029f9bb
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-02-14 09:38:06 +0100

    Update UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,22 +9,56 @@ use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class UserController extends AbstractController
 {
-    #[Route(['/profil/{username}', "/me"], name: 'get_profile')]
-    public function index(
-        // #[MapEntity(
-        //     expr: 'repository.findOneBy({"username": username}, {})',
-        //     message: 'The product does not exist'
-        // )]
-        string $username,
-        EntityManagerInterface $entityManager
+    #[Route(['/profil/{username}'], name: 'get_profile')]
+    public function profile(
+        #[MapEntity(
+            expr: 'repository.findOneBy({ "username": username }, {})',
+            message: 'The user does not exist'
+        )]
+        ?User $user
     ): Response {
-        $user = $entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
-        if ($username === "me") {
-            $user = $this->getUser();
+        dd($user);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No product found for id '
+            );
         }
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route(['/profil/{username}/albums'], name: 'get_member_albums')]
+    public function memberAlbums(
+        #[MapEntity(
+            expr: 'repository.findOneBy({ "username": username }, {})',
+            message: 'The user does not exist'
+        )]
+        ?User $user
+    ): Response {
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No product found for id '
+            );
+        }
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/me', name: 'get_my_profile')]
+    public function my_profile(
+        #[CurrentUser] ?User $user
+    ): Response {
+
+        if (!$user) {
+            throw $this->createNotFoundException();
+        }
+        // $user = $this->getUser();
 
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
