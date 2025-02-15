c55662fdb4dd799f776dd89441267deb14d4be35
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-02-15 22:34:48 +0100

    Allow user to close modal

--- assets/logged-user.js
+++ assets/logged-user.js
@@ -1,6 +1,14 @@
-const loggedDialog = document.querySelector("[data-logged-dialog]");
+const loggedDialog = document.querySelector('[data-logged-dialog]');
 
 loggedDialog.showModal();
-document.querySelector("[data-open-logged-dialog]").addEventListener("click", (e) => {
-    loggedDialog.showModal();
-})
+document
+    .querySelector('[data-open-logged-dialog]')
+    .addEventListener('click', (e) => {
+        loggedDialog.showModal();
+    });
+
+document.querySelectorAll('[data-close-dialog]').forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.target.closest('dialog').close();
+    });
+});
